f4d514a8cd749823eb2d4b7cf1e7adf7fa69ddf1
Robert Dunmire III <rdunmire@i-evolve.com>
2023-09-28 10:14:16 -0400

    First commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.10/dist-packages/nvidia/cudnn/lib/
+python3 -m wyoming_faster_whisper \
+    --uri 'tcp://0.0.0.0:10300' \
+    --data-dir /data \
+    --download-dir /data "$@"
