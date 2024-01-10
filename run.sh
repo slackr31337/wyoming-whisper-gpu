a7d5c900805089c1f9e66fc0ebf3c0be27efca31
Confused-Engineer <101591048+Confused-Engineer@users.noreply.github.com>
2024-01-10 12:14:37 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ python3 -m wyoming_faster_whisper \
     --uri 'tcp://0.0.0.0:10300' \
     --data-dir /data \
     --download-dir /data "$@" \
-    --model small \
+    --model medium-int8 \
     --beam-size 5 \
     --device cuda
