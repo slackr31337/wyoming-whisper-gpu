9fead925ee4acc165c1fcf9bf0a5a2ac65de3c59
Robert Dunmire III <rdunmire@i-evolve.com>
2025-01-14 00:26:08 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,7 @@ source /app/bin/activate
 /app/bin/python3 -m  wyoming_faster_whisper \
     --uri 'tcp://0.0.0.0:10300' \
     --data-dir /data \
-    --download-dir /data "$@"
+    --download-dir /data \
+    --model "${MODEL:-tiny-int8}" \
+    --language "${LANGUAGE:-en}" \
+    "$@"
